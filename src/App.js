5a93b77f4879c2557f2cc2c62d4d94df2346ec1f
cordrink <masodavictorjudicael@gmail.com>
2024-08-30 02:50:40 +0100

    Page d'inscription

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="form_box">
+                <div className="left_part">
+                    <div className="content_left">
+                        <h3>Welcome back</h3>
+
+                        <p>Already have an account?, click below <br/> to continue managing your files</p>
+
+                        <a href={"#"}>Sign In</a>
+                    </div>
+                </div>
+
+                <div className="right_part">
+                    <h2>Create Account</h2>
+
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor="name"><i className='bx bx-user'></i></label>
+                            <input type="text" id={"name"} placeholder={"Username"}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email"><i className='bx bx-envelope'></i></label>
+                            <input type="email" id={"email"} placeholder={"Email"}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="contact"><i className='bx bx-phone'></i></label>
+                            <input type="text" id={"contact"} placeholder={"Contact"}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="pwd"><i className='bx bx-lock-alt'></i></label>
+                            <input type="text" id={"pwd"} placeholder={"Password"}/>
+                        </div>
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
